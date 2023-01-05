f89755b39ad07604fd70dabb1339dfcab211ab26
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2023-01-05 22:28:06 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_long_description():
 
 
 setup(name='huskytools',
-      version='0.1.0',
+      version='0.2.0',
       description='Tools for interacting with the HuskyLens AI camera',
       long_description=get_long_description(),
       long_description_content_type='text/markdown',
