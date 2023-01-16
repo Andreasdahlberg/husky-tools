548bc4050a59c3184acdef61a15b9200c2180ded
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2023-01-16 22:34:43 +0100

    Improve docstrings

--- huskytools/huskylens.py
+++ huskytools/huskylens.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 
-"""Interface for the HuskyLens object recognition module."""
+"""Module implementing a low level API for the Huskylens camera.
+
+"""
 
 import math
 import logging
@@ -12,7 +14,17 @@ logger = logging.getLogger(__name__)
 
 
 class RecognitionAlgorithm:
-    """Class representing the algorithms available on the HuskyLens."""
+    """Class representing the algorithms available on the HuskyLens.
+
+     Attributes:
+        FACE_RECOGNITION: Recognize and track faces.
+        OBJECT_TRACKING: Track a single learned object.
+        OBJECT_RECOGNITION: Recognize and track several pre-defined objects.
+        LINE_TRACKING: Track lines, several lines of different color can be tracked.
+        COLOR_RECOGNITION: Recognize and track colors.
+        TAG_RECOGNITION: Detect and track April Tags.
+        OBJECT_CLASSIFICATION: Recognize learned objects.
+    """
     FACE_RECOGNITION = 0x00
     OBJECT_TRACKING = 0x01
     OBJECT_RECOGNITION = 0x02
@@ -34,32 +46,32 @@ class Block:
 
     @property
     def x(self) -> int:
-        """Get the x coordinate of the center of the block."""
+        """X coordinate of the center of the block."""
         return self._x
 
     @property
     def y(self) -> int:
-        """Get the y coordinate of the center of the block."""
+        """Y coordinate of the center of the block."""
         return self._y
 
     @property
     def width(self) -> int:
-        """Get the width of the block in pixels."""
+        """Width of the block in pixels."""
         return self._width
 
     @property
     def height(self) -> int:
-        """Get the height of the block in pixels."""
+        """Height of the block in pixels."""
         return self._height
 
     @property
     def id(self) -> int:
-        """Get the ID of the block."""
+        """Object ID of the block."""
         return self._id
 
     @property
     def learned(self) -> bool:
-        """Check if the block is learned."""
+        """If the block is learned."""
         return self.id > 0
 
 
@@ -75,32 +87,32 @@ class Arrow:
 
     @property
     def x_tail(self) -> int:
-        """Get the x coordinate of the tail of the arrow."""
+        """X coordinate of the tail of the arrow."""
         return self._x_tail
 
     @property
     def y_tail(self) -> int:
-        """Get the y coordinate of the tail of the arrow."""
+        """Y coordinate of the tail of the arrow."""
         return self._y_tail
 
     @property
     def x_head(self) -> int:
-        """Get the x coordinate of the head of the arrow."""
+        """X coordinate of the head of the arrow."""
         return self._x_head
 
     @property
     def y_head(self) -> int:
-        """Get the y coordinate of the head of the arrow."""
+        """Y coordinate of the head of the arrow."""
         return self._y_head
 
     @property
     def id(self) -> int:
-        """Get the ID of the arrow."""
+        """Object ID of the arrow."""
         return self._id
 
     @property
     def learned(self) -> bool:
-        """Check if the arrow is learned."""
+        """If the block is learned."""
         return self.id > 0
 
     def get_angle(self) -> float:
@@ -113,15 +125,34 @@ class Arrow:
 
 
 class ChecksumMismatch(Exception):
+    """Exception raised when a checksum does not match the expected value."""
     pass
 
 
 class ResponseLengthError(Exception):
+    """Exception raised when the response length does not match the expected value."""
     pass
 
 
 class Interface:
-    """Class for the HuskyLens object recognition module."""
+    """Serial port interface for the Huskylens camera.
+
+    Args:
+        port: Serial port connected to the Huskylens camera.
+        baudrate: Baudrate, make sure that this matches the baudrate configured on the Huskylens.
+        timeout: Timeout in seconds.
+
+
+    Raises:
+        ChecksumMismatch: If the checksum returned from the Huskylens is incorrect.
+        ResponseLengthError: If the response from the Huskylens is to short or long.
+
+    Examples:
+        Here is an example of how to create a Huskylens interface object:
+        >>> from huskytools import huskylens
+        >>> with huskylens.Interface("/dev/ttyUSB0") as lens:
+        ...     pass
+    """
     _COMMAND_HEADER_ADDRESS = [0x55, 0xAA, 0x11]
     _COMMAND_REQUEST_KNOCK = 0x2C
     _COMMAND_REQUEST_ALGORITHM = 0x2D
@@ -149,7 +180,15 @@ class Interface:
         self._serial = serial.Serial(port, baudrate=baudrate, timeout=timeout)
 
     def knock(self) -> bool:
-        """Check if the HuskyLens is connected."""
+        """Check if the HuskyLens is connected.
+
+        Returns:
+            True if the Huskylens is connected and responding.
+
+        Examples:
+            >>> print(lens.knock())
+            True
+        """
         logger.info('COMMAND_REQUEST_KNOCK')
         self._write_command(self._COMMAND_REQUEST_KNOCK)
         try:
@@ -159,64 +198,128 @@ class Interface:
         return self._is_response_ok(response)
 
     def set_algorithm(self, algorithm: int) -> bool:
-        """Set the algorithm used by the HuskyLens."""
+        """Set the algorithm used by the HuskyLens.
+
+        Args:
+            algorithm: ID of the algorithm to set.
+
+        Returns:
+            True if algorithm was changed successfully.
+
+        Examples:
+            >>> print(lens.set_algorithm(huskylens.RecognitionAlgorithm.FACE_RECOGNITION))
+            True
+        """
         logger.info('COMMAND_REQUEST_ALGORITHM %d', algorithm)
         self._write_command(self._COMMAND_REQUEST_ALGORITHM, algorithm.to_bytes(2, byteorder='little'))
         response = self._read_response()
         return self._is_response_ok(response)
 
     def learn(self, object_id: int) -> bool:
-        """Learn the current recognized object with the given ID."""
+        """Learn the current recognized object with the given ID.
+
+        Args:
+            object_id: ID for the object to learn (1-1023).
+
+        Returns:
+            True if object was learned successfully.
+        """
         logger.info('COMMAND_REQUEST_LEARN %d', object_id)
         self._write_command(self._COMMAND_REQUEST_LEARN, object_id.to_bytes(2, byteorder='little'))
         response = self._read_response()
         return self._is_response_ok(response)
 
     def forget(self) -> bool:
-        """Forget learned objects for the current algorithm."""
+        """Forget learned objects for the current algorithm.
+
+        Returns:
+            True if the learned objects was forgotten successfully.
+        """
         logger.info('COMMAND_REQUEST_FORGET')
         self._write_command(self._COMMAND_REQUEST_FORGET)
         response = self._read_response()
         return self._is_response_ok(response)
 
-    def get_blocks(self) -> list:
-        """Get a list of blocks from the HuskyLens."""
+    def get_blocks(self) -> list(Block):
+        """Get a list of blocks from the HuskyLens.
+
+        Returns:
+            A list of all blocks.
+
+        Examples:
+            >>> lens.get_blocks()
+            [<Block>, <Block>]
+        """
         logger.info('COMMAND_REQUEST_BLOCKS')
         self._write_command(self._COMMAND_REQUEST_BLOCKS)
         return self._handle_block_response()
 
     def get_blocks_learned(self) -> list:
-        """Get a list of learned blocks from the HuskyLens."""
+        """Get a list of learned blocks from the HuskyLens.
+
+        Returns:
+            A list of all learned blocks.
+        """
         logger.info('COMMAND_REQUEST_BLOCKS_LEARNED')
         self._write_command(self._COMMAND_REQUEST_BLOCKS_LEARNED)
         return self._handle_block_response()
 
-    def get_blocks_by_id(self, id: int) -> list:
-        """Get a list of blocks with a specific ID from the HuskyLens."""
+    def get_blocks_by_id(self, id: int) -> list(Block):
+        """Get a list of blocks with a specific ID from the HuskyLens.
+
+        Args:
+            id: ID of requested block.
+
+        Returns:
+            List of blocks with `id`.
+        """
         logger.info('COMMAND_REQUEST_BLOCKS_BY_ID %d', id)
         self._write_command(self._COMMAND_REQUEST_BLOCKS_BY_ID, id.to_bytes(2, byteorder='little'))
         return self._handle_block_response()
 
-    def get_arrows(self) -> list:
-        """Get a list of arrows from the HuskyLens."""
+    def get_arrows(self) -> list(Arrow):
+        """Get a list of arrows from the HuskyLens.
+
+        Returns:
+            A list of all arrows.
+        """
         logger.info('COMMAND_REQUEST_ARROWS')
         self._write_command(self._COMMAND_REQUEST_ARROWS)
         return self._handle_arrow_response()
 
-    def get_arrows_learned(self) -> list:
-        """Get a list of learned arrows from the HuskyLens."""
+    def get_arrows_learned(self) -> list(Arrow):
+        """Get a list of learned arrows from the HuskyLens.
+
+        Returns:
+            A list of all learned arrows.
+        """
         logger.info('COMMAND_REQUEST_ARROWS_LEARNED')
         self._write_command(self._COMMAND_REQUEST_ARROWS_LEARNED)
         return self._handle_arrow_response()
 
-    def get_arrows_by_id(self, id: int) -> list:
-        """Get a list of arrows with a specific ID from the HuskyLens."""
+    def get_arrows_by_id(self, id: int) -> list(Arrow):
+        """Get a list of arrows with a specific ID from the HuskyLens.
+
+        Args:
+            id: ID of requested arrow.
+
+        Returns:
+            List of arrows with `id`.
+        """
         logger.info('COMMAND_REQUEST_ARROWS_BY_ID %d', id)
         self._write_command(self._COMMAND_REQUEST_ARROWS_BY_ID, id.to_bytes(2, byteorder='little'))
         return self._handle_arrow_response()
 
     def photo(self) -> bool:
-        """Take a photo with the HuskyLens and save to the SD-card."""
+        """Take a photo with the HuskyLens and save to the SD-card.
+
+        Returns:
+            True if the command was successful.
+
+        Note:
+            The Huskylens will return `success` even if no SD-card is inserted but an error message
+            will be displayed on the screen.
+        """
         logger.info('COMMAND_REQUEST_PHOTO')
         self._write_command(self._COMMAND_REQUEST_PHOTO)
         response = self._read_response()
@@ -225,15 +328,31 @@ class Interface:
     def screenshot(self) -> bool:
         """Save a screenshot of the current UI to the SD-card.
 
-        Note that the function will return True even if no SD-card is inserted.
+        Returns:
+            True if the command was successful.
+
+        Note:
+            The Huskylens will return `success` even if no SD-card is inserted but an error message
+            will be displayed on the screen.
         """
         logger.info('COMMAND_REQUEST_SAVE_SCREENSHOT')
         self._write_command(self._COMMAND_REQUEST_SAVE_SCREENSHOT)
         response = self._read_response()
         return self._is_response_ok(response)
 
-    def set_name(self, name: str, object_id: int):
-        """Set a custom name for the object with given ID."""
+    def set_name(self, name: str, object_id: int) -> bool:
+        """Set a custom name for the object with given ID.
+
+        The custom name will be displayed on the Huskylens screen if an object with `object_id`
+        is recognized.
+
+        Args:
+            name: The name to be set for the object
+            object_id: The id of the object whose name is to be set
+
+        Returns:
+            True if the name was set successfully.
+        """
         logger.info('COMMAND_REQUEST_CUSTOM_NAMES')
 
         data = bytearray()
@@ -247,7 +366,19 @@ class Interface:
         return self._is_response_ok(response)
 
     def set_text(self, text: str, x: int, y: int) -> bool:
-        """Set a custom text on the HuskyLens display."""
+        """Set a custom text on the HuskyLens display.
+
+        Set a custom text of max 19 characters that will be displayed on the Huskylens screen. Max
+        10 texts can be displayed at the same time, new texts above this limit will replace old texts.
+
+        Args:
+            text: Text to display.
+            x: The X coordinate for the text (0-320).
+            y: The Y coordinate for the text (0-240).
+
+        Returns:
+            True if the text was set successfully.
+        """
         logger.info('COMMAND_REQUEST_CUSTOM_TEXT')
 
         # Make sure that the text position is on the screen.
@@ -275,28 +406,62 @@ class Interface:
         return self._is_response_ok(response)
 
     def clear_text(self) -> bool:
-        """Clear all custom texts on the HuskyLens display."""
+        """Clear all custom texts on the HuskyLens display.
+
+        Returns:
+            True if the text was cleared successfully.
+        """
         logger.info('COMMAND_REQUEST_CLEAR_TEXT')
         self._write_command(self._COMMAND_REQUEST_CLEAR_TEXT)
         response = self._read_response()
         return self._is_response_ok(response)
 
-    def save_model(self, file_number):
-        """Save the current algorithms model to the SD-card."""
+    def save_model(self, file_number: int) -> bool:
+        """Save the current algorithms model to the SD-card.
+
+        The file will be saved as `<algorithm>_Backup_<file_number>.conf`, e.g.
+        `FaceRecognition_Backup_0.conf`.
+
+        Args:
+            file_number: File number used to generate the file name for the model.
+
+        Returns:
+            True if the command was successful.
+
+        Note:
+            The Huskylens will return `success` even if no SD-card is inserted but an error message
+            will be displayed on the screen.
+        """
+
         logger.info('COMMAND_REQUEST_SEND_KNOWLEDGES')
         self._write_command(self._COMMAND_REQUEST_SEND_KNOWLEDGES, file_number.to_bytes(2, byteorder='little'))
         response = self._read_response()
         return self._is_response_ok(response)
 
-    def load_model(self, file_number):
-        """Load a model from the SD-card."""
+    def load_model(self, file_number: int) -> bool:
+        """Load a model from the SD-card.
+
+        See [save_model][huskytools.huskylens.Interface.save_model] for more information
+        about models.
+
+        Args:
+            file_number: File number used to generate the file name of the model to load.
+
+        Note:
+            The Huskylens will return `success` even if no SD-card is inserted but an error message
+            will be displayed on the screen.
+        """
         logger.info('COMMAND_REQUEST_RECEIVE_KNOWLEDGES')
         self._write_command(self._COMMAND_REQUEST_RECEIVE_KNOWLEDGES, file_number.to_bytes(2, byteorder='little'))
         response = self._read_response()
         return self._is_response_ok(response)
 
     def is_pro(self) -> bool:
-        """Check if the HuskyLens is a pro version."""
+        """Check if the HuskyLens is a pro version.
+
+        Returns:
+            True if pro.
+        """
         logger.info('COMMAND_REQUEST_IS_PRO')
         self._write_command(self._COMMAND_REQUEST_IS_PRO)
         response = self._read_response()
@@ -350,7 +515,7 @@ class Interface:
     def __exit__(self, *args):
         self._serial.close()
 
-    def _handle_block_response(self) -> list:
+    def _handle_block_response(self) -> list(Block):
         """Handle a block response from the HuskyLens."""
         info_response = self._read_response()
         number_of_blocks = int.from_bytes(info_response[5:6], byteorder='little', signed=False)
@@ -368,7 +533,7 @@ class Interface:
 
         return blocks
 
-    def _handle_arrow_response(self) -> list:
+    def _handle_arrow_response(self) -> list(Arrow):
         """Handle an arrow response from the HuskyLens."""
         info_response = self._read_response()
         number_of_arrows = int.from_bytes(info_response[5:6], byteorder='little', signed=False)
